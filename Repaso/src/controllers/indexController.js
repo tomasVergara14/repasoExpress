aed667ab64a278f83abef045ab0ab66828a6f344
tomasVergara14 <tomasvergara272@gmail.com>
2021-04-28 12:59:09 -0300

    login

--- Repaso/src/controllers/indexController.js
+++ Repaso/src/controllers/indexController.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const bcrypt = require('bcrypt');
 
 const hobbies = ['patinar','jugar','apuestas','programar','cucharear']
 
@@ -20,26 +21,39 @@ const indexController = {
             lastname: req.body.lastname,
             dni: req.body.dni,
             mail:req.body.mail,
-            pass: req.body.pass
+            pass: bcrypt.hashSync(req.body.pass, 12) //Para encriptar la contraseña
+                                                //para leerla y compararla es bcrypt.compareSync(textoPlano, hash)
         }
 
-        //Volverla json
+        //Leer nuestro archivo para ver si habia usuarios registrados
 
         let archivoUsuarios = fs.readFileSync('usuariosInfo.json', {encoding:'utf-8'})
-        let usuarios = [];
+        
+        //Declaro la variable por fuera por el scope
+        
+        var usuarios ; 
+
+        //Reviso si el archivo esta vacio declaro que va a ser un array
 
         if(archivoUsuarios ==''){
-             usuarios = []
+            usuarios = []
         }else{
-            let usuarios = JSON.parse(archivoUsuarios)
+
+            //En caso de haber algo descomprimo lo que hay y lo vuelvo un objeto para trabajar
+            usuarios = JSON.parse(archivoUsuarios)
         };
+
+        //Una vez que tenemos el archivo de usuario hecho objeto, le agregamos la info nueva para actualizarlo
 
         usuarios.push(info);
 
+        //Lo volvemos un JSON nuevamente para que podamos escribirlo con writeFileSync
 
-         usuariosJson = JSON.stringify(usuarios);
+        usuariosJson = JSON.stringify(usuarios);
 
-        fs.appendFileSync('usuariosInfo.json', usuariosJson)
+        //Lo escribimos en el archivo que definamos donde vamos a guardar los usuarios
+
+        fs.writeFileSync('usuariosInfo.json', usuariosJson)
 
         res.send(info)
     },
@@ -59,10 +73,35 @@ const indexController = {
 
     },
     login:(req,res)=>{
-        
-        let info = req.body
-        res.send(info)
 
+        var info={
+            mail: req.body.mail,
+            pass: req.body.pass
+        }
+        
+        //Leo el archivo en busca de si hay usuarios
+        let archivoUsuarios = fs.readFileSync('usuariosInfo.json', {encoding:'utf-8'})
+        
+        //declaro la variable por el scope
+        var usuarios;
+        if(archivoUsuarios == ""){
+            usuarios = []   //si esta vacio declaro que es un array
+        }else{
+            usuarios = JSON.parse(archivoUsuarios); //si tiene contenido lo descomprimo en un array
+        }
+
+        //Recorro el array de usuarios 
+        
+        for( let i = 0; i < usuarios.length ; i++ ){
+
+            //Comparo si los datos del formulario son iguales a los almacenados en el JSON
+
+            if(info.mail == usuarios[i].mail && bcrypt.compareSync(info.pass, usuarios[i].pass )){
+                
+                res.render('index', { title: 'Repaso', style:'style', hobbies: hobbies })
+            }
+            res.send('error')            
+        }
     }
 }
 
